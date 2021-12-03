a6187b25f6c1d64a13f30ec220d6b9eddb46e8a0
VABIII <tbengtson82@gmail.com>
2021-12-03 14:52:03 -0600

    Tests passing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,7 +1,8 @@
 import React from "react";
 import MutationObserver from 'mutationobserver-shim';
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
@@ -9,7 +10,20 @@ test("renders without errors", () => {
     render(<CheckoutForm/>)
 });
 
-test("shows success message on submit with form details", () => {});
+test("shows success message on submit with form details", async() => {
+    render(<CheckoutForm/>)
+
+    const submitButton = screen.getByRole("button")
+    userEvent.click(submitButton)
+
+    const successMessageOutput = await screen.findByTestId(/successMessage/i)
+
+    expect(successMessageOutput).toBeInTheDocument()
+
+
+
+
+});
 
 
 
